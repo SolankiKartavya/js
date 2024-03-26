0be545269f9602472ae630127b28d53b4bfe745d
SolankiKartavya <145988275+SolankiKartavya@users.noreply.github.com>
2024-03-26 11:08:41 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversion_Op.js
@@ -0,0 +1,15 @@
+let score = "33fdg"
+
+//console.log(typeof score);
+
+let ValueInNumber = Number(score)
+
+//console.log(typeof ValueInNumber);
+
+// "33" = 33
+// "33gfn" = NaN
+//true = 1; flase=0;
+
+let log = 1
+let bool = Boolean(log)
+console.log(bool);
