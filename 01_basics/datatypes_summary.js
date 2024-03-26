f2ef787b03a839058c43516a349ce5830f0e7fad
SolankiKartavya <145988275+SolankiKartavya@users.noreply.github.com>
2024-03-26 11:41:04 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreVal = 100.3
+
+const isLog = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const BigNumber = 748785432154478888n
+
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+
+const heros = ["shaktiman","spider"]
+let myObj = {
+    age:22,
+    name: "sk"
+}
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
+console.log(typeof BigNumber);
