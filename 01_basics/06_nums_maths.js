0bb183bc4728bc52e226a92d4f979d22bca31c02
SolankiKartavya <145988275+SolankiKartavya@users.noreply.github.com>
2024-03-26 12:23:32 +0000

    Math nad num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,29 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNum = 123.6648
+//console.log(otherNum.toPrecision(4));
+
+const hundre = 100000
+//console.log(hundre.toLocaleString('en-IN'));
+
+// ******************** Maths *******************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) ) +min)
