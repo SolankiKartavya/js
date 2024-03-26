b6dd26a781c598a59a513bbe94d6d0db9af56695
SolankiKartavya <145988275+SolankiKartavya@users.noreply.github.com>
2024-03-26 10:58:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 114455
+let accountEmail = "a@gmail.com"
+var accountPass = "4512"
+accountCity = "Rajkot"
+
+// accountId  = 2
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello") we are using node not browser
+
+console.log( "sk");
+let name = "Kartavya"
+let age = 20
+let isLoggedIn = false
