f0d11d70ab9f886fd58e4d250445d8a8b448c49b
SolankiKartavya <145988275+SolankiKartavya@users.noreply.github.com>
2024-03-27 09:04:18 +0000

    03_objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+//singleton
+//object.create
+//obj literals
+
+const JsUser = {
+    name:"Ska",
+    age:20,
+    email: "sk@gmasil.com",
+    isLog: false,
+    lastLog: ["Sunday","Monday"]
+}
+console.log(JsUser.email);
+console.log(JsUser["email"]);
