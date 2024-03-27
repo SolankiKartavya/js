5d007bfd2727385b4398d68e7ba560879835d69f
SolankiKartavya <145988275+SolankiKartavya@users.noreply.github.com>
2024-03-27 12:36:11 +0000

    while DoWhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(index);
+//     index = index + 2
+// }
+
+// let myArra = ["flash","bat","spider"]
+// let arr = 0
+// while (arr < myArra.length) {
+//     console.log(myArra[arr]);
+//     arr = arr + 1
+// }
+
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=10);
